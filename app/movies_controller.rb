8fe3bc9eeba84e78a935f13c022056b5190d1d76
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-01 23:13:44 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.find_by(attributes)  
 end
 
-def can_be_created_in_a_block(m)
+def can_be_created_in_a_block(m = {title: "Home Alone", release_date: 1990})
     m = Movie.create do |mov|
     mov.title = "The Room" 
     mov.release_date = 2003 
